3c33cc8eb68d9685e0b732ee68d6b3a6a4615e47
Lawrence Ng <lsng@usfca.edu>
2023-02-24 12:41:58 -0800

    preliminarily done, need to fix play again function

--- wordle.c
+++ wordle.c
@@ -5,40 +5,114 @@
 #include <time.h>
 
 
-void playGame(char *guess){
+int playGame(char *guess){
 
-    printf("%s\n", guess);
+    printf("The word to guess is: %s\n", guess);
     char guessWord[6];
-    strcpy(guessWord, guess);
+    strncpy(guessWord, guess, 5);
+    guessWord[5] = '\0';
     printf("%s\n", guessWord);
     
-    for(int i = 0; i < 6; i++){
-        // char guess[100];
-        // printf("Enter your guess: ");
-        // scanf("%s", guess);
+    char userGuess[6];
+    int usedChar[26] = { 0 };
+    bool containsInvalidChars = false;
+    bool notValidWord = true;
 
-        // if(strlen(guess) != 5){
-        //     printf("That is not 5 characters");
-        //     continue;
-        // }
+    for(int i = 6; i > 0; i--){
+        while((strlen(userGuess) != 5) || containsInvalidChars || notValidWord){
+            containsInvalidChars = false;
+            notValidWord = true;
+            printf("Enter your guess: ");
+            scanf("%s", userGuess);
+            printf("user guess: %s\n", userGuess);
+            if(strlen(userGuess) != 5){
+                printf("That is not 5 characters, please try again... \n");
+            }
+            if(strlen(userGuess) == 5){
+                printf("valid length");
+                for (int j = 0; j < 5; j++){
+                    if (userGuess[j] < 97 || userGuess[j] > 122){
+                        printf("this guess contains invalid characters, try again...\n");
+                        containsInvalidChars = true;
+                        break;
+                    }
+                }
+                userGuess[5] = '\0';
+            }
+            if(strlen(userGuess) == 5 && !containsInvalidChars){
+                FILE *validWordFile = fopen("./valid_words", "r");
+                char line[10];
+                // check if it's a valid word against dictionary
+                while(fgets(line, 10, validWordFile) != NULL){
+                    if(strstr(line, userGuess) == 0){
+                        printf("it's a valid word\n");
+                        notValidWord = false;
+                        break;
+                    } 
+                }
+                if (notValidWord) {
+                    printf("Your guess is not a word in my dictionary, try again... \n");
+                }
+                fclose(validWordFile);
+            }  
+        }
 
-        // printf("                       ");
-        // for(int j = 0; j < 5; j++){
-        //     if(target[j] == guess[j]){
-        //         printf("o");
-        //     } else if(strchr(target, guess[j]) != NULL){
-        //         printf("a");
-        //     } else {
-        //         printf("x");
-        //     }
-        // }
-        // puts("");
+        
 
-        // if(strncmp(target, guess, 5) == 0){
-        //     printf("You win!\n");
-        //     break;
-        // }
+        printf("Your guess is: %s\n", userGuess);
+        // printf("userGuess Length is: %d\n", strlen(userGuess));
+
+        // input letters in guess into array of used letters
+
+
+        // print letters used
+        int usedCharLength = sizeof(usedChar) / sizeof(int);
+        for (int j = 0; j < 5; j++){
+            int index = userGuess[j] - 97;
+            // printf("userGuess[] is %s\n", userGuess[j]);
+            // printf("Index is: %d\n", index);
+            if(usedChar[index] == 0){
+                usedChar[index] = 1;
+            }
+        }
+        
+        // print out guess word result
+        printf("               ");
+        for(int j = 0; j < 5; j++){
+            // might still be not working right
+            // printf("GuessWord length is : %d \n", strlen(guessWord));
+            // printf("GuessWord: %s\n", guessWord);
+            // printf("userGuess: %s\n", userGuess);
+            if(guessWord[j] == userGuess[j]){
+                printf("o");
+            } else if(strchr(guessWord, userGuess[j]) != NULL){
+                printf("!");
+            } else {
+                printf("x");
+            }
+            // puts("");
+        }
+        puts("");
+
+        printf("The characters used so far: \n");
+        for (int j = 0; j < usedCharLength; j++){
+            if (usedChar[j] == 1){
+                printf("%c ", j + 97);
+            }
+        }
+        puts("");
+
+        if(strncmp(guessWord, userGuess, 5) == 0){
+            
+            return 0;
+            
+        } else {
+            strcpy(userGuess, "");
+            printf("You have %d guess(es) remaining!\n", i);
+        }
     }
+
+    return 1;
 }
 
 
@@ -112,21 +186,52 @@ int main(void)
 
     // pick 1 word randomly 
     // set that to guess
-    char guess[6];
+    char wordToGuess[6];
     count = 1;
-    while (fgets(line, 500, readFile) != NULL){
+    while (fgets(line, 10, readFile) != NULL){
         if (count == randomNumber){
-            strcpy(guess, line);
+            strncpy(wordToGuess, line, 5);
         }
         count++;
     }
 
-    printf("word to guess is: %s\n", guess);
+    printf("word to guess is: %shello\n", wordToGuess);
+    for(int i = 0; i < strlen(wordToGuess); i++){
+        // printf("WordToGuess[%d] is %c\n", i, wordToGuess[i]);
+    }
+    printf("guess length is: %d\n", strlen(wordToGuess));
+    wordToGuess[5] = '\0';
+    for(int i = 0; i < strlen(wordToGuess); i++){
+        // printf("WordToGuess[%d] is %c\n", i, wordToGuess[i]);
+    }
+    // printf("guess length is: %d\n", strlen(wordToGuess));
     
     fclose(readFile);
 
-    playGame(guess);    
+    bool play = true;
+    while (play){
+        char response;
+        int result = playGame(wordToGuess); 
+        if(result == 0){
+            printf("You win!\n");
+        } else {
+            printf("you lose\n");
+        }
+        printf("want to play again? [Y/N] --> ");
 
+        // to fix
+        scanf(" %c", response);
+        printf("this is response: %c\n", response);
+    
+        if (response != 89 || response != 121){
 
-    // }
+            play = false;
+        }
+    }
+       
+    printf("done with game, closing....\n");
+    
+
+    //
+
 }
